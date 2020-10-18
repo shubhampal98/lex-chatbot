d12365cf901ba2c4d01c6226e5f2cc2108a22779
Shubham Pal <shubhampal1898@gmail.com>
2020-10-18 20:05:27 +0530

    lex chatbot

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Amplify from "aws-amplify";
+import {AmplifyChatbot} from "@aws-amplify/ui-react";
+import awsconfig from "./aws-exports";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+Amplify.configure(awsconfig);
+
+const App = () => (
+  <AmplifyChatbot
+    botName="OrderFlowers_dev"
+    botTitle="My ChatBot"
+    welcomeMessage="Hello, how can I help you?"
+  />
+);
 
 export default App;
